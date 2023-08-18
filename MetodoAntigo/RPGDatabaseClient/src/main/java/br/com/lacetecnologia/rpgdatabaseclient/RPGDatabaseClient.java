829e400d9e39346e2b78fa67ed95076b0728e924
Leticia Sena <leticia@prosistema.cc>
2023-08-18 13:31:09 -0300

    Inserção do projeto client desktop

---
+++ MetodoAntigo/RPGDatabaseClient/src/main/java/br/com/lacetecnologia/rpgdatabaseclient/RPGDatabaseClient.java
@@ -0,0 +1,14 @@
+
+
+package br.com.lacetecnologia.rpgdatabaseclient;
+
+/**
+ *
+ * @author leticiasena
+ */
+public class RPGDatabaseClient {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
